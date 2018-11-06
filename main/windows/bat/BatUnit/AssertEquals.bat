2c4200ff91eeb75dd6362ff86342ca0d9350fcc7
ikuyani <ikuyani2017@gmail.com>
2018-11-06 20:02:13 +0900

    empty

--- main/windows/bat/BatUnit/AssertEquals.bat
+++ main/windows/bat/BatUnit/AssertEquals.bat
@@ -1,7 +1,22 @@
 @setlocal
 @echo off
 
-rem 検証：一致
+rem ******************************************************************
+rem * AssertEquals.bat: 2つの値(期待値と実際の値)が一致するか検証する
+rem *
+rem * [説明]
+rem *   2つの値が一致する場合、Successfulをコンソールに出力し、0を返す。
+rem *   2つの値が一致しない場合、Failedをコンソールに出力し、1を返す。
+rem * 
+rem * [引数]
+rem *   %1: テスト名
+rem *   %2: 期待値
+rem *   %3: 実際の値
+rem * 
+rem * [バージョン]
+rem *   1.0
+rem ******************************************************************
+rem echo AssertEquals.bat %1 %2 %3
 
 set TEST_NAME=%~1
 set EXPECTED=%2
