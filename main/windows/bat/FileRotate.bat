b56e4a4160e95fcdb863b3fa33321d31cb983e0b
ikuyani <ikuyani2017@gmail.com>
2018-11-13 13:39:54 +0900

    msg

--- main/windows/bat/FileRotate.bat
+++ main/windows/bat/FileRotate.bat
@@ -5,37 +5,102 @@ rem ******************************************************************
 rem * FileRotate.bat: ファイルローテート処理 
 rem * 
 rem * [説明]
+rem *   ファイルもしくはフォルダに対し、世代番号を付与して名前を変更する。
+rem *   世代番号は小さい番号ほど新しく、大きい番号ほど古くなる。
+rem * 
+rem *   ＜仕様・ルール＞
+rem *   ・ファイルとフォルダに対してローテートする
+rem *   ・ローテートする対象が存在しない場合、何もしない
+rem *   ・バックアップ世代数より大きい番号のファイルに対しては何もしない
+rem *   ・バックアップ世代数は1以上を指定する
+rem * 
+rem *   ＜戻り値＞
+rem *   ・正常に処理した場合、0を返す
+rem *   ・ローテートするファイルが存在しない場合、1を返す
+rem *   ・引数が不正の場合、8を返す
+rem *   ・その他、予期しないエラーなどの場合、9を返す
 rem * 
 rem * [引数]
-rem *   %1: ローテートするファイルのあるフォルダ
-rem *   %2: ローテートするファイル名
-rem *   %3: 世代数の上限
+rem *   %1: ローテートするファイル(必須)
+rem *   %2: バックアップ世代数の上限(必須)
 rem * 
 rem * [バージョン]
-rem *   1.0.0
+rem *   1.1.0
 rem ******************************************************************
-rem echo FileRotate.bat %1 %2 %3
+rem echo FileRotate.bat %1 %2
+
+rem 利用する外部バッチファイル
+set BAT_FILEEXISTS="%~dp0\FileExists.bat"
+set BAT_DIRECTORYEXISTS="%~dp0\DirectoryExists.bat"
+set BAT_ISINTEGER="%~dp0\IsInteger.bat"
 
 rem ファイル名と世代番号の区切り文字
 set DOT=.
 
-for /l %%i in (%3,-1,1) do (
-    set _FILE=%~2%DOT%%%i
+set FILE_PATH=%~1
+set MAX_BACKUP_NUMBER=%2
+
+rem 引数チェック(引数が不正の場合、8を返す)
+if not defined FILE_PATH (
+    endlocal
+    exit /b 8
+)
+
+rem バックアップ世代数の数字判定
+call %BAT_ISINTEGER% %MAX_BACKUP_NUMBER%
+if %ERRORLEVEL% neq 0 (
+    endlocal
+    exit /b 8
+)
+rem バックアップ世代数は1以上を指定する
+if %MAX_BACKUP_NUMBER% lss 1 (
+    endlocal
+    exit /b 8
+)
+
+
+rem ローテートするファイルもしくはフォルダが存在しない場合、1を返す
+if not exist "%FILE_PATH%" (
+    endlocal
+    exit /b 1
+)
+
+rem 世代数の上限の番号からローテート処理する
+for /l %%i in (%MAX_BACKUP_NUMBER%,-1,1) do (
+    set _FILE="%FILE_PATH%%DOT%%%i"
     if exist !_FILE! (
-        if %%i equ %3 (
+        if %%i equ %MAX_BACKUP_NUMBER% (
             rem ファイルの世代番号が最大の場合、ファイルを削除する
-            del /Q  "%~1\!_FILE!" >nul
+            call %BAT_DIRECTORYEXISTS% !_FILE!
+            if !ERRORLEVEL! equ 0 (
+                rem フォルダの場合
+                rd /S /Q !_FILE! >nul
+            ) else (
+                rem ファイルの場合
+                del /Q  !_FILE! >nul
+            )
+            if !ERRORLEVEL! neq 0 (
+                endlocal
+                exit /b 9
+            )
         ) else (
             rem ファイルの世代番号が最大ではない場合、世代をアップする
             set /a _NEXT_COUNT=%%i+1 
-            move /Y %~1\!_FILE! %~1\%~2%DOT%!_NEXT_COUNT! >nul
+            move /Y !_FILE! "%FILE_PATH%%DOT%!_NEXT_COUNT!" >nul
+            if !ERRORLEVEL! neq 0 (
+                endlocal
+                exit /b 9
+            )
         )
     )
 )
 
 rem 最新のファイルは世代番号に1を付ける
-set _FILE=%~2%
-move /Y "%~1\!_FILE!" "%~1\%~2%DOT%1" >nul
+move /Y "%FILE_PATH%" "%FILE_PATH%%DOT%1" >nul
+if %ERRORLEVEL% neq 0 (
+    endlocal
+    exit /b 9
+)
 
 endlocal
-exit /b
+exit /b 0
--- main/windows/bat/FileRotateSize.bat
+++ main/windows/bat/FileRotateSize.bat
@@ -5,23 +5,68 @@ rem ******************************************************************
 rem * FileRotateSize.bat: ファイルサイズによるファイルローテート
 rem * 
 rem * [説明]
+rem *   ファイルのバイト数が上限値より大きい場合、ローテートする。
+rem * 
+rem *   ＜仕様・ルール＞
+rem *   ・ファイルに対してのみローテートする
+rem *   ・ローテートする対象が存在しない場合、何もしない
+rem *   ・ファイルのバイト数の上限は1以上を指定する
+rem *   ・バックアップ世代数など基本的な仕様はFileRotate.batに準ずる
 rem * 
 rem * [引数]
-rem *   %1: ローテートするファイルのあるフォルダ
-rem *   %2: ローテートするファイル名
-rem *   %3: 世代数の上限
-rem *   %4: ローテートの判断をするファイルのバイト数
+rem *   %1: ローテートするファイル(必須)
+rem *   %2: バックアップ世代数の上限(必須)
+rem *   %3: ファイルのバイト数上限
 rem * 
 rem * [バージョン]
 rem *   1.0.0
 rem ******************************************************************
-rem echo FileRotateSize %1 %2 %3 %4
+rem echo FileRotateSize %1 %2 %3
 
-for %%i in (%~1\%~2) do (
+rem 利用する外部バッチファイル
+set BAT_FILEEXISTS="%~dp0\FileExists.bat"
+set BAT_ISINTEGER="%~dp0\IsInteger.bat"
+set BAT_FILEROTATE="%~dp0\FileRotate.bat"
+
+set FILE_PATH=%~1
+set MAX_BACKUP_NUMBER=%2
+set MAX_BYTESIZE=%3
+
+rem 引数チェック(引数が不正の場合、8を返す)
+if not defined FILE_PATH (
+    endlocal
+    exit /b 8
+)
+
+rem ファイルのバイト数上限の数字判定
+call %BAT_ISINTEGER% %MAX_BYTESIZE%
+if %ERRORLEVEL% neq 0 (
+    endlocal
+    exit /b 8
+)
+rem ファイルのバイト数上限は1以上を指定する
+if %MAX_BYTESIZE% lss 1 (
+    endlocal
+    exit /b 8
+)
+rem ローテートするファイルが存在しない場合、1を返す
+call %BAT_FILEEXISTS% "%FILE_PATH%"
+if %ERRORLEVEL% neq 0 (
+    endlocal
+    exit /b 1
+)
+
+rem ファイルのバイト数が上限値より大きい場合、ローテートする
+for %%i in ("%FILE_PATH%") do (
     rem echo %%~zi
-    if %%~zi gtr %4 (
-        call FileRotate.bat %1 %2 %3
+    if %%~zi gtr %MAX_BYTESIZE% (
+        call %BAT_FILEROTATE% "%FILE_PATH%" %MAX_BACKUP_NUMBER%
+        if !ERRORLEVEL! neq 0 (
+            endlocal
+            exit /b !ERRORLEVEL!
+        ) 
     )
 )
 
-exit /b
+endlocal
+exit /b 0
